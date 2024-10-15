cab6ad46c2aeeb224fd573b883547c1f77fcd287
Jadeiin <92222981+Jadeiin@users.noreply.github.com>
2024-10-16 00:52:26 +0800

    feat: Add scripts for setting up VNC and ROS in tmux session.

---
+++ tools/ros.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Define the tmux session name
+SESSION="ros"
+export DISPLAY=:1
+
+# Create a new tmux session but do not attach to it
+tmux new-session -d -s $SESSION
+
+# Split the tmux window into four panes
+tmux split-window -h
+tmux split-window -v
+tmux select-pane -t 0
+tmux split-window -v
+
+# Execute the commands in the split windows
+tmux send-keys -t $SESSION:0.0 'roscore' C-m
+tmux send-keys -t $SESSION:0.1 'cd autodl-tmp && sleep 15 && rosbag play -l 2024-09-26-19-19-48.bag' C-m
+tmux send-keys -t $SESSION:0.2 'cd catkin_ws && source devel/setup.bash && rosrun sam_fp samros.py "door"' C-m
+tmux send-keys -t $SESSION:0.3 'cd catkin_ws && source devel/setup.bash && rosrun sam_fp pcd_processing_node' C-m
+
+# Attach to the tmux session
+tmux attach-session -t $SESSION
---
+++ tools/vnc.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Define the tmux session name
+SESSION="vnc"
+export DISPLAY=:1
+
+# Create a new tmux session but do not attach to it
+tmux new-session -d -s $SESSION
+
+# Split the tmux window into four panes
+tmux split-window -h
+tmux split-window -v
+tmux select-pane -t 0
+tmux split-window -v
+
+# Execute the commands in the split windows
+tmux send-keys -t $SESSION:0.0 'Xvfb "$DISPLAY" -screen 0 1600x900x24' C-m
+sleep 1
+tmux send-keys -t $SESSION:0.1 'fluxbox' C-m
+tmux send-keys -t $SESSION:0.2 'x11vnc -display "$DISPLAY" -bg -nopw -listen localhost -xkb' C-m
+tmux send-keys -t $SESSION:0.3 'rviz' C-m
+
+# Attach to the tmux session
+tmux attach-session -t $SESSION
