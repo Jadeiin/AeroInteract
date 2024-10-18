855a0c1e7a2be0a756be6faf55fd942b2cf13b02
Jadeiin <92222981+Jadeiin@users.noreply.github.com>
2024-10-18 13:56:23 +0800

    update ros.sh

--- tools/ros.sh
+++ tools/ros.sh
@@ -15,8 +15,8 @@ tmux split-window -v
 
 # Execute the commands in the split windows
 tmux send-keys -t $SESSION:0.0 'roscore' C-m
-tmux send-keys -t $SESSION:0.1 'cd autodl-tmp && sleep 15 && rosbag play -l 2024-09-26-19-19-48.bag' C-m
-tmux send-keys -t $SESSION:0.2 'cd catkin_ws && source devel/setup.bash && rosrun sam_fp samros.py "door"' C-m
+tmux send-keys -t $SESSION:0.1 'cd autodl-tmp && sleep 5 && rosbag play -l 2024-09-26-19-19-48.bag' C-m
+tmux send-keys -t $SESSION:0.2 'cd catkin_ws && source devel/setup.bash && rosrun sam_fp samros.py "[a door]"' C-m
 tmux send-keys -t $SESSION:0.3 'cd catkin_ws && source devel/setup.bash && rosrun sam_fp pcd_processing_node' C-m
 
 # Attach to the tmux session
