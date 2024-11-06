cd70d5eea56ce437cd33dd91ed2c3468bc2c49ed
Jadeiin <92222981+Jadeiin@users.noreply.github.com>
2024-11-06 21:45:51 +0800

    update ros.sh

--- tools/ros.sh
+++ tools/ros.sh
@@ -9,17 +9,18 @@ tmux setenv LD_LIBRARY_PATH $LD_LIBRARY_PATH:/root/TensorRT-10.5.0.18/lib
 tmux setenv HF_ENDPOINT https://hf-mirror.com
 tmux setenv DISPLAY :1
 
-# Split the tmux window into four panes
-tmux split-window -h
-tmux split-window -v
-tmux select-pane -t 0
-tmux split-window -v
+# Create four tmux windows
+tmux new-window -t $SESSION:1
+tmux new-window -t $SESSION:2
+tmux new-window -t $SESSION:3
+tmux new-window -t $SESSION:4
 
-# Execute the commands in the split windows
-tmux send-keys -t $SESSION:0.0 'roscore' C-m
-tmux send-keys -t $SESSION:0.1 'cd autodl-tmp && sleep 5 && rosbag play -l 2024-09-26-19-19-48.bag' C-m
-tmux send-keys -t $SESSION:0.2 'cd catkin_ws && source devel/setup.bash && rosrun sam_fp samros.py "[a door]"' C-m
-tmux send-keys -t $SESSION:0.3 'cd catkin_ws && source devel/setup.bash && rosrun sam_fp pcd_processing_node' C-m
+# Execute the commands in each window
+tmux send-keys -t $SESSION:0 'roscore' C-m
+tmux send-keys -t $SESSION:1 'cd autodl-tmp && sleep 5 && rosbag play -l 2024-09-26-19-19-48.bag' C-m
+tmux send-keys -t $SESSION:2 'cd catkin_ws && source devel/setup.bash && rosrun sam_fp samros.py "[a door]"' C-m
+tmux send-keys -t $SESSION:3 'cd catkin_ws && source devel/setup.bash && rosrun sam_fp pcd_processing_node' C-m
+tmux send-keys -t $SESSION:4 'cd catkin_ws && source devel/setup.bash && rosrun sam_fp wall_detection_node' C-m
 
 # Attach to the tmux session
 tmux attach-session -t $SESSION
